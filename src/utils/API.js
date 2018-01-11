10c4de908c69083567ed933ab3d98566f6977a05
Hunter <edhunter83@gmail.com>
2018-01-11 08:37:14 -0500

    updated API file

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,27 +2,27 @@ import axios from "axios";
 export default {
   // Saves new control group data to the database
   newData: function(data) {
-    return axios.post("/api/new_data", data);
+    return axios.post("https://pathfinder-server.herokuapp.com/api/new_data", data);
   },
   // Saves new treatment group data to the database
   // newTreatment: function(data) {
   //   return axios.post("/api/treatment", data);
   // },
   newExperiment: function(data) {
-  	return axios.post("/api/experiments", data);
+  	return axios.post("https://pathfinder-server.herokuapp.com/api/experiments", data);
   },
   // Get data
   getExperiments: function(data) {
-  	return axios.get("/api/experiments/"+data);
+  	return axios.get("https://pathfinder-server.herokuapp.com/api/experiments/"+data);
   },
   // getTreatmentData: function(data) {
   // 	return axios.get("/api/treatment", data);
   // },
   // User Login
   loginUser: function(data) {
-  	return axios.get("/api/login", data);
+  	return axios.get("https://pathfinder-server.herokuapp.com/api/login", data);
   },
   registerUser: function(data) {
-  	return axios.post("/api/register", data);
+  	return axios.post("https://pathfinder-server.herokuapp.com/api/register", data);
   }
 };
